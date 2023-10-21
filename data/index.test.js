aca597847fa130724661ca3fe730f435cd0fb38b
Erimed33 <erikamedina@pursuit.org>
2023-10-21 19:28:23 -0400

    booleans

--- data/index.test.js
+++ data/index.test.js
@@ -5,25 +5,19 @@ const {
     updatePurchase,
     deletePurchase,
     calculateTotalDonation,
-  } = require("./your-module-name");
+  } = require("./your-module-name")
   
   describe("createPurchase", () => {
     it("should create a purchase and return it", () => {
-      const purchase = createPurchase("Test Item", 10.0, 1.5);
+      const purchase = createPurchase("Test Item", 10.0, 1.5)
       expect(purchase).toEqual([
         { id: expect.any(String), name: "Test Item", amount: 10.0, donation: "1.50" },
-      ]);
-    });
-  });
+      ])
+    })
+  })
 
-  describe("getAllPurchases", () => {
-    it("should return an array of all purchases with id and name", () => {
-      const purchases = getAllPurchases();
-      expect(purchases).toEqual(expect.arrayContaining([expect.objectContaining({ id: expect.any(String), name: expect.any(String) })]));
-    });
-  });
 
-  describe('getPurchaseById', () => {
+  describe('getAllPurchases', () => {
     it("should return an array of all purchases with id and name", () => {
         const purchases = getAllPurchases()
         expect(purchases).toEqual(expect.arrayContaining([expect.objectContaining({ id: expect.any(String), name: expect.any(String)})]))
@@ -33,15 +27,41 @@ const {
 
   describe("getPurchaseById", () => {
     it("should return the purchase with the given ID", () => {
-      const purchase = createPurchase("Test Item", 10.0, 1.5);
-      const foundPurchase = getPurchaseById(purchase[0].id);
-      expect(foundPurchase).toEqual(purchase[0]);
-    });
+      const purchase = createPurchase("Test Item", 10.0, 1.5)
+      const foundPurchase = getPurchaseById(purchase[0].id)
+      expect(foundPurchase).toEqual(purchase[0])
+    })
   
     it("should return null if the purchase with the given ID does not exist", () => {
       const foundPurchase = getPurchaseById("nonexistent-id");
-      expect(foundPurchase).toBeNull();
+      expect(foundPurchase).toBeNull()
+    })
+  })
+
+  describe("updatePurchase", () => {
+    it("should update and return the purchase with the given ID", () => {
+      const purchase = createPurchase("Test Item", 10.0, 1.5);
+      const updatedPurchase = updatePurchase(purchase[0].id, "Updated Item", 20.0, 2.0)
+      expect(updatedPurchase).toEqual({ id: purchase[0].id, name: "Updated Item", amount: 20.0, donation: "2.00" })
+    })
+  
+    it("should return null if the purchase with the given ID does not exist", () => {
+      const updatedPurchase = updatePurchase("nonexistent-id", "Updated Item", 20.0, 2.0);
+      expect(updatedPurchase).toBeNull();
     });
   });
   
+  
+  describe("deletePurchase", () => {
+    it("should delete the purchase with the given ID and return true", () => {
+      const purchase = createPurchase("Test Item", 10.0, 1.5);
+      const result = deletePurchase(purchase[0].id);
+      expect(result).toBe(true);
+    });
+  
+    it("should return false if the purchase with the given ID does not exist", () => {
+      const result = deletePurchase("nonexistent-id");
+      expect(result).toBe(false);
+    });
+  });
 

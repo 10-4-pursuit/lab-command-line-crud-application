79856d74d049b5942ef1fa91fe923c49d9afb818
ada lovelace <williambookman@pursuit.org>
2023-10-28 18:01:36 -0400

    added amount and amount donate parameter to create and function plus updated index to reflect changes

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const inform = console.log;
+const inform = console.log; // whenever you call this function, it will log a message to the console
+const { readJSONFile, writeJSONFile } = require("./helpers"); //this function can be used to read and write JSON files
+const { createPurchases } = require("./src/create"); //this function can be used to create a new purchase
 
+//this function is responsible or reading the purchases from the JSON file, performing the requested action on the purchases, and then writing the updated purchases back to the JSON file
 function run() {
-    const action = process.argv[2];
-    const purchase = process.argv[3];
-    switch (action) {
-      case "index":
-        inform(action);
-        break;
-      case "create":
-        inform(action, purchase);
-        break;
-      case "show":
-        inform(action, purchase);
-        break;
-      case "update":
-        inform(action, purchase);
-        break;
-      case "delete":
-        inform(action, purchase);
-        break;
-      default:
-        inform("There was an error.");
-    }
+  let writeToFile = false;
+  let updatedPurchases = [];
+  let purchases = readJSONFile("./data", "purchases.json");
+  const action = process.argv[2];
+  const purchase = process.argv[3];
+  const purchaseAmount = process.argv[4];
+  const donateAmount = process.argv[5];
+
+  //This switch statement takes the action passed to the run() function as its input and performs the requested action on the purchases.
+  switch (action) {
+    case "index":
+      inform(action, purchases);
+      break;
+    case "create":
+      // inform(action, purchases);
+      updatedPurchases = createPurchases(purchases, purchase, purchaseAmount, donateAmount);
+      writeToFile = true;
+      inform(action, purchases);
+      break;
+    case "show":
+      inform(action, purchases);
+      break;
+    case "update":
+      inform(action, purchases);
+      break;
+    case "delete":
+      inform(action, purchases);
+      break;
+    default:
+      inform("There was an error.");
   }
-  run();
+  //This conditional statement checks if the writeToFile flag is set to true. If it is, then the updated purchases are written back to the JSON file.
+  if (writeToFile) {
+    writeJSONFile("./data", "purchases.json", updatedPurchases);
+  }
+}
+run();
--- src/create.js
+++ src/create.js
@@ -1,1 +1,9 @@
 const { nanoid } = require("nanoid")
+
+function createPurchases(purchases, purchaseName, amount, amountDonate ) {
+    const purchase = { id: nanoid(4), name: purchaseName, amount: `$${parseFloat(amount).toFixed(2)}`, donate:`$${Math.round(amountDonate * 100) / 100}` };
+    purchases.push(purchase);
+    return purchases;
+  }
+
+  module.exports = {createPurchases}
